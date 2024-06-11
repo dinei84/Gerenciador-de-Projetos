6696fbe7d1c95edf1aaba53ba85b0f2931e6253d
devdinei <devdinei@gmail.com>
2024-06-11 00:29:05 -0300

    Avançando em Criar Projetos, pronto e funcionando

--- gerenciador/src/App.js
+++ gerenciador/src/App.js
@@ -1,29 +1,28 @@
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
-import Container from './components/pages/layout/Container';
-import Navbar from './components/pages/layout/Navbar';
-import Footer from './components/pages/layout/Footer';
+import Projects from './components/pages/Projects';
+
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-      
-        <Navbar />
-
-      <Container customClass='min_height'>
+      <Navbar />
+      <Container customClass='min-height'>
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/company' element={<Company />}/>
-          <Route path='/contact' element={<Contact />}/>
-          <Route path='/newproject' element={<NewProject  />}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newproject' element={<NewProject />} />
         </Routes>
       </Container>
-
-      <Footer />>
-
+      <Footer />
     </Router>
   );
 }
